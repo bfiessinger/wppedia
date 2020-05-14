d3134a81673cfb3d34a529ebce0b99e3275d4aac
elinoo <elinoo.nguyen@gmail.com>
2020-05-14 12:20:00 +0200

    Update archive tpl

--- views/view-archive.php
+++ views/view-archive.php
@@ -2,12 +2,25 @@
 
 get_header();
 
+?>
+
+<div class="wrapper page-section">
+  <section id="main" class="site-main" role="main" itemscope="" itemprop="mainContentOfPage" itemtype="https://schema.org/WebPageElement">
+    <div class="contentwrap">
+      
+      <header>
+				<h1><?php echo __('Glossary', 'messring'); ?></h1>
+				<?php get_search_form();?>
+      </header>
+
+<?php
 /**
  * wp_wiki_tpl_initial_nav hook
  *
  * @hooked wp_wiki_tpl_initial_nav -  10
  *
  */
-do_action('wp_wiki_tpl_initial_nav');
+
+ do_action('wp_wiki_tpl_initial_nav');
 
 get_footer();
