75f566870e42758db5e5d84ae668b6b28d852f4f
elinoo <elinoo.nguyen@gmail.com>
2020-05-14 15:51:04 +0200

    more styles

--- views/view-archive.php
+++ views/view-archive.php
@@ -5,13 +5,15 @@ get_header();
 ?>
 
 <div class="wrapper page-section">
-  <section id="main" class="site-main" role="main" itemscope="" itemprop="mainContentOfPage" itemtype="https://schema.org/WebPageElement">
+  <section id="main" role="main" itemscope="" itemprop="mainContentOfPage" itemtype="https://schema.org/WebPageElement">
     <div class="contentwrap">
       
       <header>
 				<h1><?php echo __('Glossary', 'messring'); ?></h1>
 				<?php get_search_form();?>
       </header>
+
+
 
 <?php
 /**
@@ -20,14 +22,21 @@ get_header();
  * @hooked wppedia_tpl_initial_nav -  10
  *
  */
-do_action( 'wppedia_tpl_initial_nav' );
+ do_action( 'wppedia_tpl_initial_nav' ); ?>
 
+
+<?php
 /**
  * wppedia_tpl_list_entries hook
  * 
  * @hooked wppedia_tpl_list_entries - 10
  * 
  */
-do_action( 'wppedia_tpl_list_entries' );
+do_action( 'wppedia_tpl_list_entries' ); ?>
 
+		</div>
+	</section>
+</div>
+
+<?php
 get_footer();
