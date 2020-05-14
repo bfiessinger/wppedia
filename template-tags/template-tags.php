79f40f56b3e5063bd025cdbf1955ad0051a21233
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-05-14 12:00:25 +0200

    Added entrylist on archive page 📃

--- template-tags/template-tags.php
+++ template-tags/template-tags.php
@@ -7,9 +7,9 @@
 /**
  * Template hook used to display the "Initial Character Navigation"
  */
-if ( ! function_exists( 'wp_wiki_tpl_initial_nav' ) ) {
+if ( ! function_exists( 'wppedia_tpl_initial_nav' ) ) {
 
-  function wp_wiki_tpl_initial_nav() { ?>
+  function wppedia_tpl_initial_nav() { ?>
 
 	<nav id="glossary-char-navigation">
 		<ul>
@@ -39,9 +39,57 @@ if ( ! function_exists( 'wp_wiki_tpl_initial_nav' ) ) {
   <?php }
 
 }
-add_action( 'wp_wiki_tpl_initial_nav', 'wp_wiki_tpl_initial_nav', 10 );
+add_action( 'wppedia_tpl_initial_nav', 'wppedia_tpl_initial_nav', 10 );
 
 /**
  * Template hook used to display a list of Glossary entries 
  * based on their initial character
  */
+if ( ! function_exists( 'wppedia_tpl_list_entries' ) ) {
+
+	function wppedia_tpl_list_entries() { ?>
+
+		<?php foreach ( wiki_utils()->get_wiki_initial_letters() as $initial ): ?>
+
+			<?php do_action( 'wppedia_tpl_list_entries_single_char', $initial ); ?>
+
+		<?php endforeach; ?>
+
+	<?php }
+
+}
+add_action( 'wppedia_tpl_list_entries', 'wppedia_tpl_list_entries', 10 );
+
+/**
+ * Template hook used to display all Glossary entries
+ * from a single initial Character
+ */
+if ( ! function_exists( 'wppedia_tpl_list_entries_single_char' ) ) {
+
+	function wppedia_tpl_list_entries_single_char( $initial ) {
+
+		$initial_query = wp_query_all_initial_letters( 
+			[
+				'posts_per_page' => -1
+			], 
+			$initial 
+		);
+
+		if ( $initial_query->have_posts() ):
+
+	?>
+
+		<span class="initial-letter"><?php echo $initial; ?></span>
+		<ul>
+			<?php while ( $initial_query->have_posts() ): the_post(); ?>
+				<li>
+					<a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+				</li>				
+			<?php endwhile; ?>
+		</ul>
+		<?php endif; ?>
+
+	<?php }
+
+}
+add_action( 'wppedia_tpl_list_entries_single_char', 'wppedia_tpl_list_entries_single_char', 10, 1 );
