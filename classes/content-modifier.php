09d06e32069583dfc64bb1bd3b9e1436ee28cb48
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-07-16 22:59:16 +0200

    Fixed a bug where the content does not get modified when the phrase to be linked is on the first or last place of a tag

--- classes/content-modifier.php
+++ classes/content-modifier.php
@@ -132,8 +132,8 @@ class wikiContent {
       if ( stripos( $content, $post->title ) !== false ) {
 
 				$link_phrase = $this->prepare_link_phrase( $post->title );
-				if ( $this->require_full_words )
-					$link_phrase = "\s$link_phrase\s";
+				if ( $this->require_full_words && 0 === preg_match( '/' . $replace_regex . '(^|\s|\>)' . $link_phrase . '(\<|\s|$)/' . $regex_flags, $content ) )
+					continue;
 
         $post_title_link = get_permalink( $post->ID );
         $content = preg_replace( '/' . $replace_regex . '(' . $link_phrase . ')/' . $regex_flags, '<a href="' . $post_title_link . '" title="$1" class="wppedia-crosslink">$1</a>', $content );
