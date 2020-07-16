9c701b74b43b8be35cbe268b1155e624b6638730
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-07-16 21:47:54 +0200

    Update Styling

--- template-tags/template-tags.php
+++ template-tags/template-tags.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'wppedia_tpl_initial_nav' ) ) {
   function wppedia_tpl_initial_nav() { ?>
 
 	<nav id="glossary-char-navigation">
-		<ul class="flex justify-center">
+		<ul class="flex justify-center bg-lightgrey list-none leading-none p-2 mb-8">
 
 		<?php foreach ( wppedia_utils()->get_wiki_initial_letters(['hide_empty' => false]) as $slug => $initial ): ?>
 
@@ -23,9 +23,9 @@ if ( ! function_exists( 'wppedia_tpl_initial_nav' ) ) {
 				$initial_term = get_term_by( 'slug', $slug, 'initialcharacter' );
 			?>
 
-				<a href="<?php echo get_post_type_archive_link('wp_pedia_term') . '#glossary-initial-' . strtoupper( \rawurlencode( $initial ) ); ?>" title="<?php echo sprintf(__('Glossary terms with initial character „%s“ (%d)', 'wppedia'), $initial, $initial_term->count); ?>"><?php echo $initial; ?></a>		
+				<a class="block px-3 py-2 bg-grey text-darkgrey" href="<?php echo get_post_type_archive_link('wp_pedia_term') . '#glossary-initial-' . strtoupper( \rawurlencode( $initial ) ); ?>" title="<?php echo sprintf(__('Glossary terms with initial character „%s“ (%d)', 'wppedia'), $initial, $initial_term->count); ?>"><?php echo $initial; ?></a>		
 				<?php else: /* Term does not exist */ ?>
-				<span><?php echo $initial; ?></span>
+				<span class="block px-3 py-2 text-grey"><?php echo $initial; ?></span>
 
 			<?php endif; ?>
 
@@ -49,7 +49,7 @@ if ( ! function_exists( 'wppedia_tpl_list_entries' ) ) {
 
 	function wppedia_tpl_list_entries() { ?>
 
-	<div class="wppedia-entries">
+	<div class="wppedia-entries sm:flex flex-wrap -mx-4 mb-4">
 
 		<?php foreach ( wppedia_utils()->get_current_initial_letters() as $initial ): ?>
 
@@ -78,12 +78,12 @@ if ( ! function_exists( 'wppedia_tpl_list_entries_single_char' ) ) {
 
 	?>
 
-	<div id="glossary-initial-<?php echo strtoupper( \rawurlencode( $initial_letter ) ); ?>">
-		<span class="initial-letter"><?php echo $initial_letter; ?></span>
-		<ul class="initial-letter-listing">
+	<div class="w-full lg:w-3/12 md:w-4/12 sm:w-6/12 m-4" id="glossary-initial-<?php echo strtoupper( \rawurlencode( $initial_letter ) ); ?>">
+		<span class="initial-letter text-primary"><?php echo $initial_letter; ?></span>
+		<ul class="initial-letter-listing list-none p-0">
 			<?php while ( $initial_query->have_posts() ): $initial_query->the_post(); ?>
 				<li>
-					<a href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
+					<a class="text-black" href="<?php echo get_permalink(); ?>"><?php echo get_the_title(); ?></a>
 				</li>				
 			<?php endwhile; ?>
 		</ul>
--- views/view-archive.php
+++ views/view-archive.php
@@ -5,23 +5,23 @@ get_header();
 ?>
 
 <main id="main" role="main">
-      
-  <header class="container">
-		<h1><?php echo __('Glossary', 'messring'); ?></h1>
-    <?php get_wppedia_searchform(); ?>
-  </header>
-
-	<?php
-
-	/**
-	 * wppedia_tpl_initial_nav hook
-	 *
-	 * @hooked wppedia_tpl_initial_nav -  10
-	 *
-	 */
-	do_action( 'wppedia_tpl_initial_nav' ); ?>
-
+			
 	<div class="container">
+		<header class="flex justify-between">
+			<h1><?php echo __('Glossary', 'wppedia'); ?></h1>
+			<?php get_wppedia_searchform(); ?>
+		</header>
+
+		<?php
+
+		/**
+		 * wppedia_tpl_initial_nav hook
+		 *
+		 * @hooked wppedia_tpl_initial_nav -  10
+		 *
+		 */
+		do_action( 'wppedia_tpl_initial_nav' ); ?>
+
 		<?php
 		/**
 		 * wppedia_tpl_list_entries hook
