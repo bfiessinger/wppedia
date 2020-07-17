ef5f41eefc73906149aab61004cf1924fac79c82
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-07-17 12:23:05 +0200

    Add is_wppedia() public function

--- inc/public-functions.php
+++ inc/public-functions.php
@@ -2,11 +2,34 @@
 
 /**
  * Public available functions and declarations
+ * 
  * @since 1.0.0
  */
 
+/**
+ * Prints the Template for the Searchform
+ * 
+ * @uses get_searchform()
+ * @see bf\wpPedia\template
+ * 
+ * @return void
+ * 
+ * @since 1.0.0
+ */
 function get_wppedia_searchform() {
-
 	wppedia_template()->get_searchform();
+}
 
+/**
+ * Returns whether the current view is from the glossary
+ * 
+ * @uses is_wiki_post_type()
+ * @see bf\wpPedia\helper
+ * 
+ * @return boolean
+ * 
+ * @since 1.0.0
+ */
+function is_wppedia() {
+	return wppedia_utils()->is_wiki_post_type();
 }
