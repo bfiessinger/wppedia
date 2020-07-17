1ddb95d7e43cdae0a6ec9f61127b8e5997d9730a
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-07-17 04:14:50 +0200

    Add Permalink Options

--- classes/admin.php
+++ classes/admin.php
@@ -40,6 +40,9 @@ class admin {
     // Setup Admin Pages
     add_action( 'cmb2_admin_init', [ $this, 'add_wiki_admin_pages' ] );
 
+		// Custom Permalinks Section
+		add_action('admin_init', [ $this, 'wppedia_permalink_settings' ], 999999 );
+
     // Sort Wiki Entries in wp_admin
     add_action( 'pre_get_posts', [ $this, 'default_wiki_entries_orderby' ] );
 
@@ -48,10 +51,10 @@ class admin {
   /**
    * Create WP Wiki Admin Pages
    *
-   * @uses: add_menu_page()
-   * @uses: add_submenu_page()
+   * @uses new_cmb2_box()
+	 * @see https://cmb2.io/
    *
-   * @since: 1.0.0
+   * @since 1.0.0
    */
   function add_wiki_admin_pages() {
 
@@ -78,6 +81,116 @@ class admin {
 				],
 			],
 		] );
+
+	}
+
+	/**
+	 * Add a custom options section to the permalinks admin screen
+	 * 
+	 * @uses add_settings_section()
+	 * 
+	 * @since 1.0.0
+	 */
+	function wppedia_permalink_settings() {
+
+		add_option( 
+			'wppedia_permalink_base', 
+			'/glossary/', 
+			'', 
+			true 
+		);
+
+		add_option( 
+			'wppedia_permalink_term_base', 
+			'/term/', 
+			'', 
+			true 
+		);
+
+		// Register glossary Base Setting
+		register_setting(
+			'permalink', 
+			'wppedia_permalink_base',
+			[ $this, 'wppedia_permalink_part_sanitize' ]
+		);
+
+		// Register glossary Term Permalink Setting
+		register_setting(
+			'permalink', 
+			'wppedia_permalink_term_base',
+			[ $this, 'wppedia_permalink_part_sanitize' ]
+		);
+
+		add_settings_section(
+			'wppedia_permalink_structure', // ID
+			__( 'WPPedia Permalinks', 'wppedia' ),// Section title
+			[ $this, 'wppedia_permalink_settings_cb' ], // Callback for your function
+			'permalink' // Location (Settings > Permalinks)
+		);
+
+		add_settings_field( 
+			'wppedia_permalink_base_setting', 
+			__( 'WPPedia base', 'wppedia' ), 
+			[ $this, 'wppedia_setting_permalink_base_cb' ], 
+			'permalink', 
+			'wppedia_permalink_structure'
+		);
+
+		add_settings_field( 
+			'wppedia_permalink_term_base_setting', 
+			__( 'WPPedia Term base', 'wppedia' ), 
+			[ $this, 'wppedia_setting_permalink_term_base_cb' ], 
+			'permalink', 
+			'wppedia_permalink_structure'
+		);
+
+		// Save options to database
+		if ( isset( $_POST['wppedia_permalink_base'] ) || isset( $_POST['wppedia_permalink_term_base'] ) ) {
+
+			check_admin_referer('update-permalink');
+
+			$option_page = 'permalink';
+
+			$capability = 'manage_options';
+			$capability = apply_filters( "option_page_capability_{$option_page}", $capability );
+
+			if ( !current_user_can( $capability ) )
+				wp_die(__('Cheatin&#8217; uh?'));
+
+			if ( isset( $_POST['wppedia_permalink_base'] ) )
+				update_option( 'wppedia_permalink_base', $_POST['wppedia_permalink_base'] );
+
+			if ( isset( $_POST['wppedia_permalink_term_base'] ) )
+				update_option( 'wppedia_permalink_term_base', $_POST['wppedia_permalink_term_base'] );
+
+		}
+
+	}
+
+	function wppedia_permalink_settings_cb() {
+		echo wpautop( __( 'If you like, you may enter custom structures for your WPPedia URLs here.', 'wppedia' ) );
+	}
+
+	function wppedia_setting_permalink_base_cb() { ?>
+		<input type="text" name="wppedia_permalink_base" value="<?php echo get_option('wppedia_permalink_base'); ?>" class="regular-text code" />
+	<?php	}
+
+	function wppedia_setting_permalink_term_base_cb() { ?>
+		<input type="text" name="wppedia_permalink_term_base" value="<?php echo get_option('wppedia_permalink_term_base'); ?>" class="regular-text code" />
+	<?php	}
+
+	function wppedia_permalink_part_sanitize( $input ) {
+
+		$sanitized = $input;
+
+		$inputLen = strlen( $input );
+		if ( \strpos($input, '/') === false || \strpos($input, '/') != 0 )
+			$sanitized = '/' . $sanitized;
+
+		if ( \strpos($input, '/', 1) != $inputLen - 1 )
+			$sanitized .= '/';
+
+		return $sanitized;
 
 	}
 
