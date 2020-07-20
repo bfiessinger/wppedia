dd6dd6d4a249e5093b4b3d5a14f2c91740825d2a
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-07-20 16:05:45 +0200

    Remove template include

--- classes/controller.php
+++ classes/controller.php
@@ -38,9 +38,6 @@ class controller {
 
   protected function __construct() {
 
-		// Include Custom Templates
-		add_filter( 'template_include', [ $this, 'template_include' ], 10 );
-
 		// Modify the default query output
 		add_filter( 'posts_join', [ $this, 'posts_join_get_posts_by_initial_letter' ], 10, 2 );
 		add_filter( 'posts_where', [ $this, 'posts_where_get_posts_by_initial_letter' ], 10, 2 );
@@ -88,70 +85,5 @@ class controller {
     return $where;
 
   }
-	
-	/**
-	 * Modify single view
-	 * 
-	 * @param string $template - the default template for the current view
-	 * 
-	 * @since 1.0.0
-	 * 
-	 * @return string - final Template to use
-	 */
-	function template_include( $template ) {
-
-		$post_type = wppedia_utils()->is_wiki_post_type();
-
-		// Bail early if the current page is not a wiki page
-		if ( ! $post_type  )
-			return $template;
-
-		// Check for single view
-		if ( is_singular('wp_pedia_term') ) {
-
-			// Don't modify the template if specified in the current Theme
-			if ( locate_template(['single-' . $post_type . '.php']) )
-				return $template;
-
-			// Check for the single Template by Post type
-			if ( $post_type == 'wp_pedia_term' ) {
-
-				$single_template = wppedia_template()->get_view(
-					'single', 
-					[], 
-					false
-				);
-				
-				return ( $single_template ) ? $single_template : $template;
-				
-			}
-
-		}
-
-		// Check for archive view
-		if ( ( is_tax('initialcharacter') || is_post_type_archive('wp_pedia_term') ) && ! is_search() ) {
-
-			// Don't modify the template if specified in the current Theme
-			if ( locate_template(['archive-' . $post_type . '.php']) )
-				return $template;
-
-			// Checkk for the archive Template by Post Type
-			if ( $post_type == 'wp_pedia_term' ) {
-
-				$archive_template = wppedia_template()->get_view(
-					'archive', 
-					[], 
-					false
-				);
-				
-				return ( $archive_template ) ? $archive_template : $template;
-
-			}
-
-		}
-
-    return $template;
-
-	}
 
 }
