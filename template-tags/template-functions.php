92ff44298d4292774b758aab787eaa1f029dbaf9
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-06 16:12:38 +0200

    Make Navigation parts filterable

--- template-tags/template-functions.php
+++ template-tags/template-functions.php
@@ -4,12 +4,23 @@
  * WPPedia Template Hooks and Filters
  */
 
-function wppedia_navigation_link( string $term_slug, string $title = null, array $classes = [], string $active_class = null ) {
+function wppedia_navigation_link( string $term_slug ) {
 
 	$output = '';
 
-	if ( ! $active_class )
-		$active_class = 'active';
+	/**
+	 * Filter for common link Classes
+	 * 
+	 * @param array $link_classes - Array with classes for all link elements
+	 */
+	$link_classes = apply_filters( 'wppedia_navigation_link__classes', [] );
+
+	/**
+	 * Filter for the active link class
+	 * 
+	 * @param string $active_class - Classname for the active element
+	 */
+	$active_class = apply_filters( 'wppedia_navigation_link__active_class', 'active' );
 
 	if ( term_exists( $term_slug, 'wppedia_initial_letter' ) ) {
 		// Get Information about the current term
@@ -18,15 +29,15 @@ function wppedia_navigation_link( string $term_slug, string $title = null, array
 		$output .= '<a href="' . get_term_link( $obj ) . '"';
 		$output .= ' title="' . sprintf(__('Glossary terms with initial character „%s“ (%d)', 'wppedia'), $obj->name, $obj->count) . '"';
 		
-		if ( $obj->term_id === get_queried_object()->term_id )
-			$classes[] = $active_class;
+		if ( isset( get_queried_object()->term_id ) && $obj->term_id === get_queried_object()->term_id )
+			$link_classes[] = $active_class;
 
-		if ( ! empty( $classes ) )
-			$output .= ' class="' . implode( ' ', $classes ) . '"';
+		if ( ! empty( $link_classes ) )
+			$output .= ' class="' . implode( ' ', $link_classes ) . '"';
 
 		$output .= '>';
 
-		$output .= $obj->name;
+		$output .= apply_filters( 'wppedia_navigation_link__name', $obj->name );
 
 		$output .= '</a>';
 
@@ -34,18 +45,18 @@ function wppedia_navigation_link( string $term_slug, string $title = null, array
 
 		$output .= '<span';
 
-		if ( ! empty( $classes ) ) {
+		if ( ! empty( $link_classes ) ) {
 			$output .= ' class="';
-			$output .= implode( ' ', $classes );
+			$output .= implode( ' ', $link_classes );
 			$output .= '"';
 		}
 
 		$output .= '>';
-		$output .= $term_slug;
+		$output .= apply_filters( 'wppedia_navigation_link__name', $term_slug );
 		$output .= '</span>';
 
 	}
 
-	return apply_filters( 'wppedia_navigation_link', $output, $term_slug, $title, $classes, $active_class );
+	return apply_filters( 'wppedia_navigation_link', $output );
 
 }
