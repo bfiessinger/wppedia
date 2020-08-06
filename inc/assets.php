f3037e2df2d2688051e31e0c5a1bb6896401fb40
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-06 16:55:40 +0200

    Check if any CSS is collected before printing the style tag for inline usage

--- inc/assets.php
+++ inc/assets.php
@@ -11,7 +11,7 @@ function wppedia_enqueue_stylesheets() {
 	if ( ! wppedia_utils()->is_wiki_post_type() )
 		return;
 
-	if ( wppedia_utils()->get_option( \bf\wpPedia\admin::$settings_general_page, 'wppedia_layout_use-inline-styles' ) == 'on' ) {
+	if ( 'on' == wppedia_utils()->get_option( \bf\wpPedia\admin::$settings_general_page, 'wppedia_layout_use-inline-styles' ) ) {
 
 		wppedia_add_inline_style( wpPediaPluginDir . 'dist/css/components_navigation.min.css' );
 
@@ -26,10 +26,14 @@ add_action( 'wp_enqueue_scripts', 'wppedia_enqueue_stylesheets' );
 
 function wppedia_print_inline_styles() {
 
-	if ( wppedia_utils()->get_option( \bf\wpPedia\admin::$settings_general_page, 'wppedia_layout_use-inline-styles' ) != 'on' )
+	if ( 'on' != wppedia_utils()->get_option( \bf\wpPedia\admin::$settings_general_page, 'wppedia_layout_use-inline-styles' ) )
 		return;
 
-	echo '<style>' . \bf\wpPedia\inline_style_collector::getInstance()->get_final_css() . '</style>';
+	$final_css = \bf\wpPedia\inline_style_collector::getInstance()->get_final_css();
+	if ( '' == $final_css )
+		return;
+
+	echo '<style>' . $final_css . '</style>';
 
 }
 add_action( 'wp_head', 'wppedia_print_inline_styles' );
