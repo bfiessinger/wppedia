561a511274e13ccdaf2d7ae01d2d1a511994c131
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-07 14:07:53 +0200

    partial file for the searchform (#16)

--- partials/partial-searchform.php
+++ partials/partial-searchform.php
@@ -6,14 +6,4 @@
 	 * @since 1.0.0
 	 */
 
-	$post_type = 'wp_pedia_term';
-  $searchUrl = get_post_type_archive_link( $post_type );
-
-?> 
-
-<form role="search" method="get" class="search-form wppedia-search" action="<?php echo $searchUrl ?>">
-  <label class="screen-reader-text"><?php _e('Search glossary', 'wppedia'); ?></label>
-  <input type="search" class="search-field" placeholder="<?php _e('Search glossary', 'wppedia'); ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php _e('Search for', 'wppedia'); ?>:" />
-  <input type="hidden" name="post_type" value="<?php echo $post_type; ?>" />
-	<input type="submit" class="search-submit" value="Search" />
-</form>
+echo wppedia_searchform(); ?>
