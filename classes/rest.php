462875f990a72a026dd00d944e588232ab9a7401
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-17 19:22:24 +0200

    save REST endpoints in variables

--- classes/rest.php
+++ classes/rest.php
@@ -15,11 +15,17 @@ defined( 'ABSPATH' ) or die();
 
 class rest extends \WP_REST_Controller {
 
+	// Public namespace
 	public $rest_namespace = null;
+
+	// Public Endpoints
+	public $rest_endpoint_posts = null;
 
 	public function __construct() {
 
 		$this->rest_namespace = 'wppedia/v/' . helper::getInstance()->get_version();
+
+		$this->rest_endpoint_posts = 'posts';
 
 	}
 
@@ -31,12 +37,30 @@ class rest extends \WP_REST_Controller {
 
 	function register_rest_routes() {
 
-		register_rest_route( $this->rest_namespace, '/posts', [
+		register_rest_route( $this->rest_namespace, '/' . $this->rest_endpoint_posts, [
 			'methods' => 'GET',
 			'callback' => function() { 
 				return helper::getInstance()->get_wiki_entry_titles(); 
 			}
 		] );
+
+	}
+
+	/**
+	 * Simple utility to obtain the Rest endpoint for WPPedia
+	 * 
+	 * @uses rest_url()
+	 * 
+	 * @param string {$endpoint}
+	 * 
+	 * @return string - the final endpoint URL
+	 * 
+	 * @since 1.0.0
+	 */
+	public function get_endpoint_url( string $endpoint = '' ) {
+
+		$url = rest_url( $this->rest_namespace . '/' . $endpoint );
+		return $url;
 
 	}
 
