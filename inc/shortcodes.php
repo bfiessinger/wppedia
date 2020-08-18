70aac9da8b7c297ee338d71727d5d91d1ad53e88
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-18 18:44:53 +0200

    Create Shortcode for WPPedia Searchform (closes #16)

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -2,13 +2,22 @@
 
 // Create Shortcode wppedia_navigation
 // Shortcode: [wppedia_navigation]
-function create_wppedianavigation_shortcode() {
+function create_wppedia_navigation_shortcode() {
 
 	ob_start();
-
-	wppedia_template()->initial_letter_navigation();
-
+	bf\wpPedia\template::getInstance()->get_char_navigation();
 	echo ob_get_clean();
 
 }
-add_shortcode( 'wppedia_navigation', 'create_wppedianavigation_shortcode' );
+add_shortcode( 'wppedia_navigation', 'create_wppedia_navigation_shortcode' );
+
+// Create Shortcode wppedia_searchform
+// Shortcode: [wppedia_searchform]
+function create_wppedia_searchform_shortcode() {
+
+	ob_start();
+	bf\wpPedia\template::getInstance()->get_search_form();
+	echo ob_get_clean();
+
+}
+add_shortcode( 'wppedia_searchform', 'create_wppedia_searchform_shortcode' );
