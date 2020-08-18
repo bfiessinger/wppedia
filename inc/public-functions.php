7176398fac9326a94a86202aafaffc3ec98804c7
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-08-18 09:16:32 +0200

    categorize public functions for better readability

--- inc/public-functions.php
+++ inc/public-functions.php
@@ -7,6 +7,10 @@
  */
 
 /**
+ * Common
+ */
+
+/**
  * Get the post type for WPPedia Posts
  * 
  * @return string
@@ -16,6 +20,30 @@
 function get_wppedia_post_type() {
 	return bf\wpPedia\post_type::getInstance()->post_type;
 }
+
+
+/**
+ * Utilities
+ */
+
+/**
+ * Returns whether the current view is from the glossary
+ * 
+ * @uses helper::is_wiki_post_type()
+ * @see bf\wpPedia\helper
+ * 
+ * @return boolean
+ * 
+ * @since 1.0.0
+ */
+function is_wppedia() {
+	return bf\wpPedia\helper::getInstance()->is_wiki_post_type();
+}
+
+
+/**
+ * Template
+ */
 
 /**
  * Prints the Template for the Searchform
@@ -44,19 +72,10 @@ function get_wppedia_searchform_attrs( array $attrs = [], bool $tostring = true 
 	return bf\wpPedia\template::getInstance()->get_search_form_attrs( $attrs, $tostring );
 }
 
+
 /**
- * Returns whether the current view is from the glossary
- * 
- * @uses helper::is_wiki_post_type()
- * @see bf\wpPedia\helper
- * 
- * @return boolean
- * 
- * @since 1.0.0
+ * Other
  */
-function is_wppedia() {
-	return bf\wpPedia\helper::getInstance()->is_wiki_post_type();
-}
 
 /**
  * Add additional inline styles
