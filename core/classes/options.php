3fc6d0ad6f8485901eff9b2fe8241e039eb94639
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2020-10-23 11:58:35 +0200

    fix a bug where slashes or other special characters are being sanitized wrong.

--- core/classes/options.php
+++ core/classes/options.php
@@ -330,8 +330,9 @@ class options {
 	<?php }
 
 	function wppedia_permalink_part_sanitize( $input ) {
-		$input = esc_html( $input );
-		$input = urlencode( $input );
+		$input = str_replace( ' ', '-', $input );
+		$input = esc_url_raw( $input );
+		
 		return $input;
 	}
 
