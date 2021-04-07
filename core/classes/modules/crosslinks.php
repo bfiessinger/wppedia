bfiessinger/wppedia

commit 63684f3338cf19168fe5c77018d363203bad77a2

use negative lookbehinds for matching strings inside HTML tags

core/classes/modules/crosslinks.php
@@ -184,7 +184,7 @@ class crosslinks {
 
 		foreach( $xpath->query($query) as $node ) {
 
-			if ( $this->require_full_words && 0 === preg_match( '/(^|\s|\>|\#|\@|\+)' . $link_phrase . '(\?|\!|\;|,|\.|\<|\s|$)/imsu', $node->wholeText ) )
+			if ( $this->require_full_words && 0 === preg_match( '/(^|\s|(?!:\>)|\#|\@|\+)' . $link_phrase . '(\?|\!|\;|,|\.|(?!:\<)|\s|$)/imsu', $node->wholeText ) )
 				continue;
 
 			$replaced = preg_replace_callback( '/' . $link_phrase . '/imsu', function( $match ) use ( $post ) {

commit 9b531226db67036537ee2e007ab3473088bc49a1

hotfix for 63684f3 which broke the require_full_words condition. HTML Tags after the word are now captured using a positive lookahead instead of a negative.

core/classes/modules/crosslinks.php
@@ -184,7 +184,7 @@ class crosslinks {
 
 		foreach( $xpath->query($query) as $node ) {
 
-			if ( $this->require_full_words && 0 === preg_match( '/(^|\s|(?!:\>)|\#|\@|\+)' . $link_phrase . '(\?|\!|\;|,|\.|(?!:\<)|\s|$)/imsu', $node->wholeText ) )
+			if ( $this->require_full_words && 0 === preg_match( '/(^|\s|(?!\>)|\#|\@|\+)' . $link_phrase . '(\?|\!|\;|,|\.|(?=\<)|\s|$)/imsu', $node->wholeText ) )
 				continue;
 
 			$replaced = preg_replace_callback( '/' . $link_phrase . '/imsu', function( $match ) use ( $post ) {