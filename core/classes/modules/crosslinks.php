2fd7a868e9ed42abb8425d6623809b2f5bc46463
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2021-04-28 15:04:48 +0200

    fix crosslinks not working in some situations This commit covers the following problems * terms at the contents start * terms inside HTML tags

--- core/classes/modules/crosslinks.php
+++ core/classes/modules/crosslinks.php
@@ -202,7 +202,7 @@ class crosslinks {
 		foreach( $xpath->query($query) as $node ) {
 
 			$regex_flags = 'imsu';
-			$regex = ($this->require_full_words) ? '/(?!^|\s|\>|\#|\@|\+)' . $link_phrase . '(?=\?|\!|\;|,|\.|\<|\s|$)/' : '/' . $link_phrase . '/';
+			$regex = ($this->require_full_words) ? '/(?<=^|\s|\>|\#|\@|\+)' . $link_phrase . '(?=\?|\!|\;|,|\.|\<|\s|$)/' : '/' . $link_phrase . '/';
 			$regex .= $regex_flags;
 
 			$replaced = preg_replace_callback( $regex, function( $match ) use ( $post ) {
