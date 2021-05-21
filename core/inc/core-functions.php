effd498d1d658c0bdc1ffd136e04d553fe78ad3a
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2021-05-21 12:22:47 +0200

    import inline_style_collector class with a use statement

--- core/inc/core-functions.php
+++ core/inc/core-functions.php
@@ -8,6 +8,8 @@
 
 defined( 'ABSPATH' ) || die();
 
+use bf\wpPedia\inline_style_collector;
+
 require_once __DIR__ . '/template-functions.php';
 require_once __DIR__ . '/page-functions.php';
 require_once __DIR__ . '/post-functions.php';
@@ -36,7 +38,7 @@ function wppedia_get_version() {
  */
 function wppedia_add_inline_style( string $handle, string $stylesheet ) {
 	
-	if ( bf\wpPedia\inline_style_collector::getInstance()->add( $handle, $stylesheet ) )
+	if ( inline_style_collector::getInstance()->add( $handle, $stylesheet ) )
 		return true;
 
 	return false;
