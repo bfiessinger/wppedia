988ad8befde10e6bc3515158b6980515db3aa0e7
Bastian Fießinger <41627893+bfiessinger@users.noreply.github.com>
2021-06-23 19:03:17 +0200

    remove build folder temporarily

--- build/vendor/composer/installed.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-namespace WPPedia\Container;
-
-return array('root' => array('pretty_version' => 'dev-master', 'version' => 'dev-master', 'type' => 'wordpress-plugin', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'reference' => '48fe0bd95b3237acf3f16d6bf00fc7a683d74b1f', 'name' => 'wppedia/wppedia', 'dev' => \true), 'versions' => array('aristath/kirki' => array('pretty_version' => '3.1.7', 'version' => '3.1.7.0', 'type' => 'library', 'install_path' => __DIR__ . '/../aristath/kirki', 'aliases' => array(), 'reference' => '0f9d24437665f6d5664da4e12bb4f0ae410815e4', 'dev_requirement' => \false), 'wppedia/wppedia' => array('pretty_version' => 'dev-master', 'version' => 'dev-master', 'type' => 'wordpress-plugin', 'install_path' => __DIR__ . '/../../', 'aliases' => array(), 'reference' => '48fe0bd95b3237acf3f16d6bf00fc7a683d74b1f', 'dev_requirement' => \false)));
