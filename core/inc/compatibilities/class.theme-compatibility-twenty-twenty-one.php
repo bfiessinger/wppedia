71a831c2a00bb28f025e39d21898bd02f5f93c1a
bfiessinger <bastianfiessinger1@yahoo.de>
2022-04-22 13:26:03 +0200

    fix: compatibility issues with the content wrapper of Twentý Twenty One

--- core/inc/compatibilities/class.theme-compatibility-twenty-twenty-one.php
+++ core/inc/compatibilities/class.theme-compatibility-twenty-twenty-one.php
@@ -22,6 +22,22 @@ class WPPedia_Twenty_Twenty_One {
 		// This theme doesn't have a traditional sidebar.
 		remove_action( 'wppedia_sidebar', 'wppedia_sidebar', 10 );
 
+		// Twenty Twenty-One wraps the content before WPPedia does it.
+		remove_action( 'wppedia_before_main_content', 'wppedia_wrapper_start', 10 );
+		remove_action( 'wppedia_after_main_content', 'wppedia_wrapper_end', 20 );
+
+		add_action('wp_enqueue_scripts', [ __CLASS__, 'unset_wppedia_content_width' ], 202 );
+
+	}
+
+	/**
+	 * Unset the theme's content width.
+	 * Twenty Twenty-One uses a custom structure to handle the content width.
+	 *
+	 * @since 1.3.0
+	 */
+	public static function unset_wppedia_content_width() {
+		wppedia_remove_inline_style('wppedia-content-width');
 	}
 
 }
