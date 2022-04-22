dac2ce61a1ffdf5dea0e8313de1547ef7e2fefee
bfiessinger <bastianfiessinger1@yahoo.de>
2022-04-22 18:25:38 +0200

    add customizer controls

--- core/classes/customize.php
+++ core/classes/customize.php
@@ -8,8 +8,7 @@
 
 namespace WPPedia;
 
-
-use \WPPedia_Vendor\Kirki;
+// use \Kirki;
 
 // Make sure this file runs only from within WordPress.
 defined( 'ABSPATH' ) or die();
@@ -18,17 +17,33 @@ class customize {
 
 	protected function __clone() {}
 
-  public function __construct() {
-		add_action( 'init', [ $this, 'kirki_init' ] );
+	public function _init() {
+		add_action( 'customize_register', [ $this, 'customize_register' ] );
+		add_action( 'customize_preview_init', [ $this, 'customize_preview_init' ] );
 	}
 
-	/**
-	 * Add Customizer Panels
-	 * 
-	 * @since 1.3.0
-	 */
-	public function kirki_init() {
+	public function customize_register( $wp_customize ) {
 
+		$wp_customize->add_section( '_wppedia_customize', [
+			'title' => __( 'WPPedia', 'wppedia' ),
+			'priority' => 1,
+		] );
+
+		$wp_customize->add_setting( 'wppedia_main_color', [
+			'default' => '#160351',
+			'transport' => 'refresh',
+		] );
+
+		$wp_customize->add_control( new \WP_Customize_Color_Control( $wp_customize, 'wppedia_main_color', [
+			'label' => __( 'Main Color', 'wppedia' ),
+			'section' => '_wppedia_customize',
+			'settings' => 'wppedia_main_color',
+		] ) );
+
+	}
+
+	public function customize_preview_init() {
+		//wp_enqueue_script( 'wppedia-customizer', get_template_directory_uri() . '/js/customizer.js', [ 'jquery', 'customize-preview' ], '', true );
 	}
 
 }
