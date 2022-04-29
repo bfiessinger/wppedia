57e20ea985a41e0bc469fe50065f644214061965
bfiessinger <bastianfiessinger1@yahoo.de>
2022-04-29 15:42:58 +0200

    fix indentation

--- templates/nav/char-navigation.php
+++ templates/nav/char-navigation.php
@@ -1,18 +1,18 @@
 <?php
 /**
  * WPPedia char navigation
- * 
+ *
  * This template can be overridden by copying it to yourtheme/wppedia/nav/char-navigation.php
- * 
+ *
  * ATTENTION!
  * In case WPPedia needs to make changes to the template files, you (the theme developer)
  * will need to copy these new template files to maintain compatibility.
- * 
+ *
  * Whenever we make changes to the template files we will bump the version and list all changes
  * in the CHANGELOG.md file.
- * 
+ *
  * Happy editing!
- * 
+ *
  * @see https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package wppedia
